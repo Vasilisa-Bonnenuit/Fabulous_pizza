bfe1e709dec273f3fe17eec6857b9f9bed03c1e3
Vasilisa-Bonnenuit <Stefanitskaya0607@gmail.com>
2026-04-16 23:36:42 +0300

    Файл для настройки бдшки

--- database.py
+++ database.py
@@ -2,12 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# строка подключения к бд
 DATABASE_URL = "sqlite:///./users.db"
 
+# создаём движок
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# фабрика сессий, через неё мы будем работать с БД (добавлять, читать, удалять)
+SessionLocal = sessionmaker(
+    autocommit=False,   # чтобы изменения не применялись автоматически
+    autoflush=False,    # чтобы не было лишних автоматических сохранений
+    bind=engine
+)
 
+# базовый класс для всех моделей
 Base = declarative_base()
